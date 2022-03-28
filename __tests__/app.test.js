50e741ccbd2de317403ac4433ec9732620b35c8d
apwb89 <apwb89@gmail.com>
2022-03-28 15:30:21 +0100

    updated tests to use async/await and add get/api/users endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,29 +12,45 @@ beforeEach(() => {
 
   describe('GET /api/topics', () => {
       describe('Functionality', () => {
-        test('Returns an array of topic objects with slug and decription properties', () => {
-        return request(app)
+        test('Returns an array of topic objects with slug and decription properties', async () => {
+        const response = await request(app)
         .get("/api/topics")
         .expect(200)
-        .then((res) => {
-            expect(res.body.topics.length).toBe(3);
-            res.body.topics.forEach(topic => {
+            expect(response.body.topics.length).toBe(3);
+            response.body.topics.forEach(topic => {
                 expect.objectContaining({
                     slug: expect.any(String),
                     description: expect.any(String),
                   });
             })
-        })
+        
       })
     })
       describe('Error Handling', () => {
-        test('Returns 404 not found if no topics are found', () => {
-            return request(app)
+        test('Returns 404 not found if no topics are found', async () => {
+            const response = await request(app)
             .get('/api/tpoics')
             .expect(404)
-            .then((res) => {
-                expect(res.body.msg).toBe('Not Found');
+            
+                expect(response.body.msg).toBe('Not Found');
             })
         })
+    })
+  
+
+  describe('GET /api/users', () => {
+      describe('Functionality', () => {
+          test('Returns an array of objects with username property', async () => {
+              const response = await request(app)
+              .get('/api/users')
+              .expect(200);
+
+              expect(response.body.usernames.length).toBe(4);
+              response.body.usernames.forEach(username => {
+                  expect.objectContaining({
+                      username: expect.any(String)
+                  })
+              })
+          })
       })
   })
--- models/app.models.js
+++ models/app.models.js
@@ -4,3 +4,8 @@ exports.getAllTopics = async () => {
     const results = await db.query('SELECT * FROM topics;');
         return results.rows
     }
+
+exports.getAllUsernames = async () => {
+    const results = await db.query('SELECT username FROM users');
+        return results.rows
+}
